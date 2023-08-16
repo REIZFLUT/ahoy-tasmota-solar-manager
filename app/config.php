af943b90653b0cf6490d10dfd6a42735c02b7a9b
REIZFLUT <info@reizflut.com>
2023-08-16 14:22:34 +0200

    Config prep for OpenDtu
    ...

--- app/config.php
+++ app/config.php
@@ -15,15 +15,29 @@ $GLOBALS['CONFIG']['EnergyTariff']              = floatval(Sqlite::selectById('s
 $GLOBALS['CONFIG']['UseVirtualFeedbackCounter'] = intval(Sqlite::selectById('system_config', 'use_virtual_feedback_counter', '*', 'k')['v']);
 $GLOBALS['CONFIG']['VirtualFeedbackCounter']    = intval(Sqlite::selectById('system_config', 'virtual_feedback_counter', '*', 'k')['v']);
 
+$GLOBALS['CONFIG']['UseDtu']                    = Sqlite::selectById('system_config', 'use_dtu', '*', 'k')['v']; 
+$GLOBALS['CONFIG']['InverterId']                = Sqlite::selectById('system_config', 'inverter_id', '*', 'k')['v']; 
+
 // Reaction when ...
 $GLOBALS['CONFIG']['MaxGridConsumtion']         = intval(Sqlite::selectById('system_config', 'max_grid_consumtion', '*', 'k')['v']);
 $GLOBALS['CONFIG']['MaxGridFeedback']           = intval(Sqlite::selectById('system_config', 'max_grid_feedback', '*', 'k')['v']);
 
 
 $GLOBALS['CONFIG']['SmartMeter']['Url']         = Sqlite::selectById('system_config', 'smartmeter.url', '*', 'k')['v'];
+$GLOBALS['CONFIG']['SmartMeter']['TotalInKey']  = 'Total_in';
+$GLOBALS['CONFIG']['SmartMeter']['TotalOutKey'] = 'Total_out';
+$GLOBALS['CONFIG']['SmartMeter']['TimeKey']     = 'Time';
+$GLOBALS['CONFIG']['SmartMeter']['PowerKey']    = 'Power_curr';
+
+
 $GLOBALS['CONFIG']['AhoyDTU']['BaseUrl']        = Sqlite::selectById('system_config', 'ahoydtu.base_url', '*', 'k')['v']; 
+
 
 $GLOBALS['CONFIG']['AhoyDTU']['InverterMax']    = intval(Sqlite::selectById('system_config', 'ahoydtu.inverter_max', '*', 'k')['v']);
 $GLOBALS['CONFIG']['AhoyDTU']['OutputMax']      = intval(Sqlite::selectById('system_config', 'ahoydtu.output_max', '*', 'k')['v']);
 $GLOBALS['CONFIG']['AhoyDTU']['OutputMin']      = intval(Sqlite::selectById('system_config', 'ahoydtu.output_min', '*', 'k')['v']); // do not set minimum lower than 20
 $GLOBALS['CONFIG']['AhoyDTU']['ReactionFactor'] = intval(Sqlite::selectById('system_config', 'ahoydtu.reaction_factor', '*', 'k')['v']); // smooth power values
+
+
+$GLOBALS['CONFIG']['OpenDTU']['User']           = Sqlite::selectById('system_config', 'opendtu.usr', '*', 'k')['v'];;
+$GLOBALS['CONFIG']['OpenDTU']['Password']       = Sqlite::selectById('system_config', 'opendtu.pwd', '*', 'k')['v'];;
